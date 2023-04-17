c14d770a84f52498ea0160f7b09e12bd72a3c6ed
HP <HP@LAPTOP-16PQ9653>
2023-04-17 12:28:52 +0200

    Zmiana

--- Wizualne_Obraz/Form1.Designer.cs
+++ Wizualne_Obraz/Form1.Designer.cs
@@ -40,6 +40,7 @@
             this.pictureBox1.Size = new System.Drawing.Size(490, 306);
             this.pictureBox1.TabIndex = 0;
             this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
             // 
             // button1
             // 
@@ -50,6 +51,7 @@
             this.button1.TabIndex = 1;
             this.button1.Text = "WCZYTAJ";
             this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // Form1
             // 
--- Wizualne_Obraz/Form1.cs
+++ Wizualne_Obraz/Form1.cs
@@ -2,9 +2,27 @@ namespace Wizualne_Obraz
 {
     public partial class Form1 : Form
     {
+
+        OpenFileDialog ofd = new OpenFileDialog();
+        
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            ofd.Filter = "BMP (*.BMP)|*.bmp";
+            if(ofd.ShowDialog() == DialogResult.OK)
+            {
+                pictureBox1.Image = new Bitmap(ofd.FileName);
+                pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+            }
         }
     }
 }
