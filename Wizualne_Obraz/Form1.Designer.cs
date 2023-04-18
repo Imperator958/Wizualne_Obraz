cd16053b5aae6576cecd92b5fa9fc9d35ac0eec2
HP <HP@LAPTOP-16PQ9653>
2023-04-18 14:09:36 +0200

    Commit_3

--- Wizualne_Obraz/Form1.Designer.cs
+++ Wizualne_Obraz/Form1.Designer.cs
@@ -30,6 +30,9 @@
         {
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
@@ -53,11 +56,47 @@
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Segoe UI", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.button2.Location = new System.Drawing.Point(599, 181);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(148, 68);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "OBRÓT O 90";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Font = new System.Drawing.Font("Segoe UI", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.button3.Location = new System.Drawing.Point(599, 266);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(148, 68);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "ODBICIE LUSTRZANE II";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Font = new System.Drawing.Font("Segoe UI", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.button4.Location = new System.Drawing.Point(599, 350);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(148, 68);
+            this.button4.TabIndex = 4;
+            this.button4.Text = "ODBICIE LUSTRZANE II";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.pictureBox1);
             this.Name = "Form1";
@@ -71,5 +110,8 @@
 
         private PictureBox pictureBox1;
         private Button button1;
+        private Button button2;
+        private Button button3;
+        private Button button4;
     }
 }
--- Wizualne_Obraz/Form1.cs
+++ Wizualne_Obraz/Form1.cs
@@ -2,9 +2,9 @@ namespace Wizualne_Obraz
 {
     public partial class Form1 : Form
     {
-
+        Bitmap image;
         OpenFileDialog ofd = new OpenFileDialog();
-        
+
         public Form1()
         {
             InitializeComponent();
@@ -20,8 +20,41 @@ namespace Wizualne_Obraz
             ofd.Filter = "BMP (*.BMP)|*.bmp";
             if(ofd.ShowDialog() == DialogResult.OK)
             {
-                pictureBox1.Image = new Bitmap(ofd.FileName);
+                image = new Bitmap(ofd.FileName);
+                pictureBox1.Image = new Bitmap(image);
                 pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if(image != null)
+            {
+                int W;
+                image.RotateFlip(RotateFlipType.Rotate90FlipNone);
+                W = pictureBox1.Width;
+                pictureBox1.Width = pictureBox1.Height;
+                pictureBox1.Height = W;
+                pictureBox1.Image = image;
+                
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if(image != null)
+            {
+                image.RotateFlip(RotateFlipType.RotateNoneFlipY);
+                pictureBox1.Image = image;
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            if(image != null)
+            {
+                image.RotateFlip(RotateFlipType.RotateNoneFlipX);
+                pictureBox1.Image = image;
             }
         }
     }
